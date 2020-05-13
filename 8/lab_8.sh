3bada7cf4c677a7e6e3aa8aed0752739a6443fa8
Anatoliy <tolikzuzin.com@gmail.com>
2020-05-13 20:19:26 +0300

    laba8

---
+++ 8/lab_8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ls /home/$USER
+ls ~/
+ls -l ~/
+ls -al ~/
+touch ~/labs/8/new.file
+echo $USER > ~/labs/8/new.file
+chmod 400 ~/labs/8/new.file
+echo $USER > ~/labs/8/new.file
+chmod 644 ~/labs/8/new.file
